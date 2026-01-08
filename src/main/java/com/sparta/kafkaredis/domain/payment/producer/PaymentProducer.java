6271e2be1842968a54143d80af48914a899f97f4
B-ChJ <bchaejoo@gmail.com>
2026-01-08 20:36:45 +0900

    docs: 기존 코드에서 변경(개선)된 내용을 Javadoc으로 추가

--- src/main/java/com/sparta/kafkaredis/domain/payment/producer/PaymentProducer.java
+++ src/main/java/com/sparta/kafkaredis/domain/payment/producer/PaymentProducer.java
@@ -9,6 +9,18 @@ import com.sparta.kafkaredis.common.model.kafka.event.PaymentCompletedEvent;
 
 import lombok.RequiredArgsConstructor;
 
+/**
+ * Kafka Producer 설정 클래스입니다.
+ * <p>
+ * 기존 코드에서 따로 지정하지 않았던 파티셔닝 전략을
+ * 주문 번호(orderId)를 기준으로 파티션이 나눠 전달되도록
+ * 두 번째 인수로 추가했습니다.
+ * </p>
+ *
+ * @author 변채주
+ * @version 1.0
+ * @since 2026. 01. 08.
+ */
 @Service
 @RequiredArgsConstructor
 public class PaymentProducer {
